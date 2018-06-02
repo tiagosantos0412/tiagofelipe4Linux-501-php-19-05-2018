d6d7514bba6ea3f859a8dfcf91980daba1839b25
Tiago Felipe dos Santos <tiagosantos0412@gmail.com>
2018-06-02 14:04:02 -0300

    Aula03 criptografando senhas hash

---
+++ aula03/func-diversas.php
@@ -0,0 +1,100 @@
+<?php 
+	echo "<pre>";
+	//verifica se a variavel esta setada
+
+	$nome = 'nome';
+	if (isset($nome)){
+		echo "Nome existe!";
+	}else {
+		echo "Nome nao existe!";
+	}
+ 		
+ 	//**************************************************************************************************
+
+	//verifica se existe um indice no array
+
+	echo "<br>";
+
+	$arr = ['nome' => 'Tiago'];
+
+	if (isset($arr['nome'])) {
+		echo "existe o indice";
+	} else {
+		echo "nao existe o indice";
+	}
+
+
+	echo "<hr>";
+
+	//**************************************************************************************************
+
+	//destroi variavel
+	$var = 'asdf';
+	unset($var);
+	echo (isset($var)) ? 'existe' : 'nao existe';
+
+	echo "<hr>";
+
+	//**************************************************************************************************
+
+	//verifica se a variavel esta vazia
+
+	//considerados vazio: 0, '', false, [], null
+	$var = '';
+
+	echo (empty($var)) ? 'vazio' : 'nao vazio';
+	echo "<hr>";
+
+	//**************************************************************************************************
+
+	//verifica se var e do tipo especifico
+
+	if(!is_array($var)){
+		echo "nao e um array";
+	}else {
+		echo "e um array";
+	}
+	echo "<hr>";
+
+	//**************************************************************************************************
+
+	//serializar uma variavel
+	$arr = ['Tiago', 'tiagosantos0412@gmail.com', 'tiagosantos0412@hotmail.com'];
+
+	$str =  serialize($arr);
+
+
+	echo "<br>";
+
+	print_r(unserialize($str));
+	echo "<hr>";
+
+	//**************************************************************************************************
+
+	//imprime uma string formatada
+	$str = 'Ola meu nome e %s';
+	printf($str, 'Tiago');
+
+	echo "<hr>";
+
+	//**************************************************************************************************
+	//elimina as tags htm de um formulario
+
+	echo "<hr>";
+	echo htmlspecialchars('<hr>');
+
+	echo "<br>";
+
+	//echo "<script>alert('voce foi invadido')</script>";
+
+	echo htmlspecialchars("<script>	alert('voce foi invadido')</script>");	
+
+	echo "<hr>";
+
+	//elimina as tags htm de um formulario
+	echo strip_tags('<hr>');
+
+	//nl2br converte uma quebra de linha console para um <br> ou quebra de linha do navegador
+	
+
+ ?>
---
+++ aula03/func-hash.php
@@ -0,0 +1,61 @@
+<?php 
+
+	//criptografando com md5
+
+	echo md5('123456');
+
+	echo "<br>";
+
+	echo md5('123456');
+
+	echo "<hr>";
+
+	//criptografando com sha1
+
+	echo sha1('123456');
+
+	echo "<br>";
+
+	echo sha1('123456');
+
+	echo "<hr>";
+
+	//criptografando escolhendo o hash
+
+	echo hash('md5','123456');
+
+	echo "<br>";
+
+	echo hash('sha1','123456');
+
+	echo "<hr>";
+
+	//criptografando com o crypt
+
+	echo crypt('123456', 'rl');
+
+	echo "<br>";
+
+	echo crypt('123456', 'rl');
+
+	echo "<hr>";
+
+	//criptografando com passoword_hash
+	echo password_hash('123456',PASSWORD_BCRYPT);
+	echo '<br>';
+	echo password_hash('123456',PASSWORD_BCRYPT);
+
+	echo '<hr>';
+
+	$senha = '123456';
+
+	$cript = password_hash($senha, PASSWORD_BCRYPT);
+
+	//verificar se a senha esta valida
+	if (password_verify($senha, $cript)) {
+		echo 'Esta Logado';
+	}
+
+
+
+ ?>
