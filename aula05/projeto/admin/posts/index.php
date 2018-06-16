4acb90b82dc7890a0558392eadab7ea1ab5ac819
Tiago Felipe dos Santos <tiagosantos0412@gmail.com>
2018-06-16 18:02:20 -0300

    Alterando o arquivo readme.md

--- aula05/projeto/admin/posts/index.php
+++ aula05/projeto/admin/posts/index.php
@@ -1,10 +1,10 @@
 <?php require_once dirname(__DIR__).'/layout/header.php'; ?>
 <?php require_once dirname(dirname(__DIR__)).'/include/data/posts/db-posts.php'; ?>
-<?php $user = getUserLogged(); ?>
+<?php $user = getUserLogged();  ?>
 
 <h2>Posts</h2>
 <?php if (findAllPostByUser($user['id'])): ?>
-	<a href="create.php" class="btn btn-primary float-right" style="margin: 5px">Novo Post</a>
+	<a href="create.php" class="btn btn-outline-primary float-right" style="margin: 5px">Novo Post</a>
 	<table class="table table-hover">
 	<thead>
 		<tr>
@@ -20,17 +20,18 @@
 		<tr>
 			<td><?= $post['id']?></td>
 			<td><?= $post['titulo']?></td>
-			<td><?= $post['publicado']?></td>
-			<td><?= $post['criado_em']?></td>
+			<td><?= ($post['publicado'] == 't') ? 'Publicado' : 'Não Publicado'?></td>
+			<td><?= substr($post['criado_em'],0,19)?></td>
 			<td>
-			<a href="edit.php?id=<?=$post['id']?>" class="btn btn-outline-info">Editar</a>
-			<a href="delete.php?id=<?=$post['id']?>" class="btn btn-outline-danger">Deletar</a></td>
+				<a href="edit.php?id=<?=$post['id']?>" class="btn btn-outline-info">Editar</a>
+				<a href="delete.php?id=<?=$post['id']?>" class="btn btn-outline-danger">Deletar</a>
+			</td>
 		</tr>
 	<?php endforeach; ?>
 	</tbody>
 	</table>
 <?php else: ?>
 	<p>Você não fez nenhum post, deseja fazer o primeiro??</p>
-	<p><a href="create.php" class="btn btn-primary" style="margin: 5px">Novo Post</a></p>
+	<p><a href="create.php" class="btn btn-outline-primary" style="margin: 5px">Novo Post</a></p>
 <?php endif; ?>
 <?php require_once dirname(__DIR__).'/layout/footer.php'; ?>
--- aula05/projeto/readme.md
+++ aula05/projeto/readme.md
@@ -3,21 +3,35 @@
 ## Blog
 
 ##Estrutura de pastas
-<p>├── admin<br>
+<p>
+├── admin<br>
+│   ├── index.php<br>
 │   ├── layout<br>
+│   │   ├── footer.php<br>
+│   │   └── header.php<br>
 │   ├── login<br>
-│   │   └── login.php<br>
+│   │   ├── login.php<br>
+│   │   └── logout.php<br>
 │   └── posts<br>
+│       ├── create.php<br>
+│       ├── delete.php<br>
+│       ├── edit.php<br>
+│       └── index.php<br>
 ├── include<br>
 │   ├── config<br>
 │   │   └── config.php<br>
-│   └── data<br>
-│       ├── db<br>
-│       │   └── connection.php<br>
-│       └── posts<br>
-│           └── db-posts.php<br>
+│   ├── data<br>
+│   │   ├── db<br>
+│   │   │   └── connection.php<br>
+│   │   ├── login<br>
+│   │   │   └── db-login.php<br>
+│   │   └── posts<br>
+│   │       └── db-posts.php<br>
+│   └── utils<br>
+│       └── session-funcs.php<br>
 ├── index.php<br>
 ├── layout<br>
 │   ├── footer.php<br>
 │   └── header.php<br>
-└── readme.md</p>
+└── readme.md<br>
+</p>
